d4d34e968230f3ccb98d290f211f55486b00f645
Damrongsak Reetanon <damrongs@gmail.com>
2021-01-22 15:15:51 +0700

    Create create_csr.sh

---
+++ create_csr.sh
@@ -0,0 +1,13 @@
+cat > user1-csr.yaml <<EOF
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: user1
+spec:
+  groups:
+  - system:authenticated
+  request: $(cat user1.csr | base64 | tr -d "\n")
+  signerName: kubernetes.io/kube-apiserver-client
+  usages:
+  - client auth
+EOF
